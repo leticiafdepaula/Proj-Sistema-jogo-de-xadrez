b7dee2fe8be2f3609573ed3c16815b71964d1889
Leticia Ferreira <Leticiaferreira150998@gmail.com>
2023-04-16 11:41:44 -0300

    BoardException and defensive programming

--- src/Boardgame/Board.java
+++ src/Boardgame/Board.java
@@ -1,6 +1,7 @@
 package Boardgame;
 
 import Chess.pieces.King;
+import Chess.pieces.King.king;
 import Chess.pieces.Rook;
 
 public class Board {
@@ -8,8 +9,14 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Piece [][] pieces;
+	private Position position;
+	private Piece piece;
 	
 public Board(int rows, int columns) {
+	if (rows < 1 || columns < 1) {
+throw new BoardException("Erro creating board: there must be at least 1 row and 1 column");		
+	
+	}
 		this.rows = rows;
 		this.columns = columns;
 	pieces = new Piece[rows][columns];
@@ -20,32 +27,60 @@ public int getRows() {
 	return rows;
 }
 
-public void setRows(int rows) {
-	this.rows = rows;
-}
+
 
 public int getColumns() {
 	return columns;
 }
-
-public void setColumns(int columns) {
-	this.columns = columns;
-}
 	
 public Piece piece(int row, int column) {
-	return pieces[row][column];
+	if (!positionExists(row, column)) {
+	throw new BoardException("Position not on the board");
+	
+  }
+	return pieces[position.getRow()][position.getColumn()] = piece;
 }
 	public Piece piece(Position position) {
-	return pieces [position.getRow()][position.getColumn()];
-		
-	}
+if (!positionExists(position)) {
+	throw new BoardException("Position not on the board");
+}
+return pieces[position.getRow()][position.getColumn()];
 
-public void placePiece(King king, Position position) {
+	}
+public void placePiece(Rook rook, Position position) {
+	if(thereIsAPiece(position))
+		throw new BoardException("There is already a piece on position " + position);
+  }
+
+private boolean positionExists(int row, int column) {
+   return row >= 0 && row < rows && column >= 0 && column < columns;
+}
+	
+public boolean positionExists(Position position) {
+  return positionExists(position.getRow(), position.getColumn());
+
 }
 
-public void placePiece(Rook rook, Position position) {
+public boolean thereIsAPiece(Position position){
+	if (!positionExists(position)) {
+		throw new BoardException("Position not on the board");
+	}
+	
+return piece(position) != null;
+
+  }
+
+public void placePiece(King king, Position position2) {
 	// TODO Auto-generated method stub
 	
 }
 
+
 }
+
+
+	
+
+
+
+
